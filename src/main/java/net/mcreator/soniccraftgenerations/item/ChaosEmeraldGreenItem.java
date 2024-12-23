a0fb9a7820a661808344eaae3bbd2a8bf5936d9c
HachiIshi <J@DESKTOP-S5LOJNP>
2024-12-23 02:08:48 -0500

    emerald tweaks.

--- src/main/java/net/mcreator/soniccraftgenerations/item/ChaosEmeraldGreenItem.java
+++ src/main/java/net/mcreator/soniccraftgenerations/item/ChaosEmeraldGreenItem.java
@@ -1,21 +1,11 @@
 
 package net.mcreator.soniccraftgenerations.item;
 
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
-
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 
 public class ChaosEmeraldGreenItem extends Item {
 	public ChaosEmeraldGreenItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
-	}
-
-	@Override
-	@OnlyIn(Dist.CLIENT)
-	public boolean isFoil(ItemStack itemstack) {
-		return true;
+		super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.COMMON));
 	}
 }
