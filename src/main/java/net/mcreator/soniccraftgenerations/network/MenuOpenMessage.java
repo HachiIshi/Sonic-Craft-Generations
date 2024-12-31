c03ccf71e7a8a6b8530c51f9ed8fdb3860f920c3
HachiIshi <J@DESKTOP-S5LOJNP>
2024-12-31 15:44:19 -0500

    Added Miles Electric, Possibly Fixed White Emerald

---
+++ src/main/java/net/mcreator/soniccraftgenerations/network/MenuOpenMessage.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.soniccraftgenerations.network;
+
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.FriendlyByteBuf;
+
+import net.mcreator.soniccraftgenerations.procedures.MilesElectricOpenProcedure;
+import net.mcreator.soniccraftgenerations.SonicCraftGenerationsMod;
+
+import java.util.function.Supplier;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MenuOpenMessage {
+	int type, pressedms;
+
+	public MenuOpenMessage(int type, int pressedms) {
+		this.type = type;
+		this.pressedms = pressedms;
+	}
+
+	public MenuOpenMessage(FriendlyByteBuf buffer) {
+		this.type = buffer.readInt();
+		this.pressedms = buffer.readInt();
+	}
+
+	public static void buffer(MenuOpenMessage message, FriendlyByteBuf buffer) {
+		buffer.writeInt(message.type);
+		buffer.writeInt(message.pressedms);
+	}
+
+	public static void handler(MenuOpenMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+		NetworkEvent.Context context = contextSupplier.get();
+		context.enqueueWork(() -> {
+			pressAction(context.getSender(), message.type, message.pressedms);
+		});
+		context.setPacketHandled(true);
+	}
+
+	public static void pressAction(Player entity, int type, int pressedms) {
+		Level world = entity.level();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		// security measure to prevent arbitrary chunk generation
+		if (!world.hasChunkAt(entity.blockPosition()))
+			return;
+		if (type == 0) {
+
+			MilesElectricOpenProcedure.execute(world, x, y, z, entity);
+		}
+	}
+
+	@SubscribeEvent
+	public static void registerMessage(FMLCommonSetupEvent event) {
+		SonicCraftGenerationsMod.addNetworkMessage(MenuOpenMessage.class, MenuOpenMessage::buffer, MenuOpenMessage::new, MenuOpenMessage::handler);
+	}
+}
